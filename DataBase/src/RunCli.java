f199f7c3b7be64161bd70369c4e91cea8b458c6a
Aiman <aiman.buhlaiqah@gmail.com>
2015-01-06 21:10:17 +0000

    Replaced files for easier execution and better filing structure

--- DataBase/src/RunCli.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-
-
-public class RunCli {
-	public static void main(String[] args) throws IOException {
-		String [] argument = {"127.0.0.1", "6789"};
-		ClientKeyboardTest.main(argument);
-	}
-}
--- DataBase/src/RunPop3.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-
-
-public class RunPop3 {
-	public static void main(String[] args) throws IOException {
-		String [] argument = {"6789", "600"};
-		Pop3Server.main(argument);
-	}
-}
