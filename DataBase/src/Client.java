3be60d8773a96ec3030bcbdbcd6ffffe50081e00
Aiman <aiman.buhlaiqah@gmail.com>
2015-01-07 19:24:58 +0000

    New implementation (new thread for reading from chat server)

--- DataBase/src/Client.java
+++ DataBase/src/Client.java
@@ -11,22 +11,15 @@ import java.net.UnknownHostException;
 public class Client {
 
 	private final boolean AUTO_FLUSH = true;
-	private final String OK = "+OK ";
-	private final String ERR = "-ERR ";
-	private final String CRLF = "\r\n";
-	private final String LOST_CONNECTION_MESSAGE = "Connection to server is lost";
 	private final String CLIENT_REQUEST_QUIT = "QUIT";
 	private String response;
 	private String request;
-	private boolean running;
 	private Socket connection;
 	private PrintWriter serverOutput;
 	private BufferedReader serverInput;
 	private PrintStream clientOutput;
 	private BufferedReader clientInput;
-	private BufferedReader clientInputMark;
-	private BufferedReader clientInputAiman;
-	private String test="Mark";
+	private boolean running;
 
 
 	public Client(String hostName, int portNumber, InputStream clientInput, PrintStream clientOutput) {
@@ -41,15 +34,13 @@ public class Client {
 			System.err.println("Couldn't get I/O for the connection to " + hostName);
 			System.exit(1);
 		}
-		running = true;
 	}
 
 	public Client(String hostName, int portNumber) {
 		this(hostName, portNumber, System.in, System.out);
 	}
 
-
-	public void startRunning() {
+	public void runClient() {
 		try {
 			interaction();
 			endOfConnection();
@@ -60,21 +51,40 @@ public class Client {
 
 	/**
 	 * start of client/server interaction
-	 * @throws IOException
+	 * reads request and forwards it to ServerThread, then returns serverThread's response 
+	 * <b>request</b> request from client input [what he typed]
+	 * <b>response</b> the response from serverThread to the client request 
+	 * @throws IOException 
 	 */
 	private void interaction() throws IOException {
-		response = serverInput.readLine();
-		clientOutput.println(response);
+		Thread responseThread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                while (!Thread.interrupted()) {
+                	try {
+                    	while ((response = serverInput.readLine()) != null) {
+                			clientOutput.println(response);
+                		}
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+                }
+            }
+        });
+        responseThread.start();
+        
+        running = true;
 		while (running) {
-			request = clientInput.readLine();				
-			response = handleRequest(request);
-			clientOutput.print(response);
-
-			//handling terminating cases
-			if (request.equals(CLIENT_REQUEST_QUIT)) {
-				running = false;
+			request = clientInput.readLine();
+			if (request != null) {
+				//send the user command to server
+				serverOutput.println(request);
+				if (request.equals(CLIENT_REQUEST_QUIT)) {
+					running = false;
+					responseThread.interrupt();
+				}
 			}
-
 		}
 	}
 
@@ -110,32 +120,4 @@ public class Client {
 		serverOutput = new PrintWriter(connection.getOutputStream(), AUTO_FLUSH);
 		serverInput = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 	}
-
-	/**
-	 * handles request and forward it to Server, then returns server's response 
-	 * @param request from client
-	 * @return response from server
-	 * @throws InterruptedException 
-	 */
-	private String handleRequest(String request) {
-		serverOutput.println(request);
-
-		try {
-			String serverResponse = "";
-			Thread.sleep(250);
-			if (serverInput.ready()) {
-				while (serverInput.ready()) {
-					serverResponse += serverInput.readLine()+CRLF;
-				}
-				return serverResponse;
-			}
-
-		} catch (IOException ioException) {
-
-		} catch (InterruptedException interruptedException) {
-
-		}
-		return null;
-	}
-
 }
