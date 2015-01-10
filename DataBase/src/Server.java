4a6d9df20cbb1c34337c4e20c7b5a8df1bcd9eb8
Aiman <aiman.buhlaiqah@gmail.com>
2015-01-10 12:41:33 +0000

    Unlogged Exception

--- DataBase/src/Server.java
+++ DataBase/src/Server.java
@@ -37,6 +37,7 @@ public class Server {
 		// when port is busy
 		} catch (IOException ioException) {
 			System.err.println("Could not listen on port " + portNumber);
+			ioException.printStackTrace();
 			System.exit(-1);
 		}
 	}
